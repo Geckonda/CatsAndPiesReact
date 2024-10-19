62149385dbc20c411859629055b75477bd3461da
Geckonda <Aquariusstd@yandex.ru>
2024-10-19 20:00:38 +0400

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import Welcome from './Welcome';
+import Card from './Components/Card';
+import styles from "./App.module.css"
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    console.log(`Количество кликов изменилось ${count}`);
+  }, [count])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className={styles.title}>Hello, React!</h1>
+      <Welcome name={name || "Sasha"}/>
+      <p>Количество кликов: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Жми на меня</button>
+      <button onClick={() => setCount(0)}>Обнулить счетчик</button>
+      <div>
+        <input
+          type='text'
+          placeholder='Введите ваше имя'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+
+      <Card title="Mobile" price={count * 3000} />
+      <Card title="Car" price="21200" />
+      <Card title="Appliacation" price="200" />
     </div>
   );
 }
---
+++ src/Welcome.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Welcome = (props) => {
+    return <h2>Привет, {props.name}!</h2>
+}
+
+export default Welcome;
